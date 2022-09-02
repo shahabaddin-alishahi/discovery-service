0919066b3e2e5d5d5c448a1dabce6b189046a7a4
shahab <shahabaddin.alishahi@gmail.com>
2022-09-02 13:59:34 +0430

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM gradle:7.5.0-jdk-alpine AS TEMP_BUILD_IMAGE
+ENV APP_HOME=/usr/app/
+WORKDIR $APP_HOME
+COPY build.gradle settings.gradle $APP_HOME
+
+COPY gradle $APP_HOME/gradle
+COPY --chown=gradle:gradle . /home/gradle/src
+USER root
+RUN chown -R gradle /home/gradle/src
+
+RUN gradle build || return 0
+COPY . .
+RUN gradle clean build
+
+# actual container
+FROM openjdk:11.0.6-buster
+ENV ARTIFACT_NAME=discovery-service-0.0.1-SNAPSHOT.jar
+ENV APP_HOME=/usr/app/
+
+WORKDIR $APP_HOME
+COPY --from=TEMP_BUILD_IMAGE $APP_HOME/build/libs/$ARTIFACT_NAME .
+
+EXPOSE 8761
+ENTRYPOINT exec java -jar ${ARTIFACT_NAME}
---
+++ src/test/java/energizeglobalservices/discovery/service/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,33 @@
+package energizeglobalservices.discovery.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	@Test
+	public void shouldStartEurekaServer() {
+		ResponseEntity<String> entity = this.testRestTemplate.getForEntity(
+				"http://localhost:" + this.port + "/eureka/apps", String.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+}
